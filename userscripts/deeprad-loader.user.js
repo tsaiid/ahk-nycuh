8399148d5cb83af6e37ab792c00286b489be215b
I-Ta Tsai @ NYCUH <ittsai@gmail.com>
2026-05-29 21:03:47 +0800

    chore: 整理 userscript 目錄
    - 將 DeepRad userscript 與 loader 從 others 移到 userscripts 目錄。
    - 更新 DeepRad loader 的本機 require 路徑，指向新的 userscripts/deeprad.user.js。
    - 移除不再需要的 EBM userscript、Stylus 樣式與 default.dic。

--- userscripts/deeprad-loader.user.js
+++ userscripts/deeprad-loader.user.js
@@ -7,7 +7,7 @@
 // @match        http://172.17.15.97:17000/
 // @match        http://172.17.15.97:17000/*
 // @run-at       document-start
-// @require      file:///D:/USER/Documents/ahk-nycuh/others/deeprad.user.js
+// @require      file:///D:/USER/Documents/ahk-nycuh/userscripts/deeprad.user.js
 // @grant        GM_registerMenuCommand
 // @grant        GM_setClipboard
 // @grant        unsafeWindow
